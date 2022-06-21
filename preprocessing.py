dd686c4718bc6ad2fbd6e16691749c9f5c2337cf
jjjk123 <jj.kubica@student.uw.edu.pl>
2022-06-21 17:47:43 +0200

    upd

---
+++ preprocessing.py
@@ -0,0 +1,33 @@
+import pandas as pd
+
+def analyse_data(anno, exp):
+    merged_df = pd.merge(anno, exp.rename(columns={'Unnamed: 0':'sample'}), on='sample',  how='left')
+    
+    to_drop = ['sample', 
+        'secondary_sample_accession', 
+        'sel_Beghini_2021', 
+        'study_accession',
+        'sample_accession',
+        'instrument_platform',
+        'instrument_model',
+        'library_layout',
+        'sample_alias',
+        'country',
+        'individual_id',
+        'timepoint',
+        'body_site',
+        'host_phenotype',
+        'host_subphenotype',
+        'class',
+        'to_exclude',
+        'low_read',
+        'low_map',
+        'excluded',
+        'excluded_comment',
+        'mgp_sample_alias',
+        'westernised',
+        'body_subsite']
+
+    df = merged_df.drop(columns=to_drop)
+    
+    return df
--- states.py
+++ states.py
@@ -1,13 +1,121 @@
-from FeatureCloud.app.engine.app import AppState, app_state
-
+from FeatureCloud.app.engine.app import AppState, app_state, Role
+import pandas as pd
+from preprocessing import analyse_data
+ 
 
 # FeatureCloud requires that apps define the at least the 'initial' state.
 # This state is executed after the app instance is started.
+
+class Data():
+    def read_data(self, file_anno, file_exp):
+        # df = pd.DataFrame()
+        anno = pd.read_csv(file_anno, delimiter=',')
+        exp = pd.read_csv(file_exp, delimiter=',')
+
+        df = analyse_data(anno, exp)
+
+        self.df = df
+    
+    def get_dataframe(self):
+        return self.df
+    
+    def delete_columns(self, to_drop):
+        self.df = self.df.drop(columns=to_drop)
+
+c = Data()
+
 @app_state('initial')
 class InitialState(AppState):
 
     def register(self):
-        self.register_transition('terminal')  # We declare that 'terminal' state is accessible from the 'initial' state.
+        self.register_transition('read', Role.BOTH)
+        
+    def run(self):
+        return 'read'
+
+@app_state('read', Role.BOTH)
+class ReadState(AppState):
+
+    def register(self):
+        self.register_transition('aggregate', Role.COORDINATOR)
+        self.register_transition('await', Role.PARTICIPANT)
+            
+    def run(self):
+        c.read_data('/mnt/input/anno.csv', '/mnt/input/exp.csv')
+
+        # find columns to drop
+
+        to_drop = c.get_dataframe().columns[(c.get_dataframe() == 0).all()]
+
+        self.send_data_to_coordinator(to_drop)
+
+        if self.is_coordinator:
+            return 'aggregate'
+        else:
+            return 'await'
+
+@app_state('aggregate', Role.COORDINATOR)
+class AggregateState(AppState):
+
+    def register(self):
+        self.register_transition('await', Role.COORDINATOR)
 
     def run(self):
-        return 'terminal'  # This means we are done. If the coordinator transitions into the 'terminal' state, the whole computation will be shut down.
+        to_drop = self.gather_data()
+
+        # find common columns to drop and broadcast them
+
+        self.broadcast_data(to_drop)
+
+        return 'await' 
+
+@app_state('await', Role.BOTH)
+class AwaitState(AppState):
+
+    def register(self):
+        self.register_transition('send_global_params', Role.COORDINATOR)
+        self.register_transition('apply', Role.PARTICIPANT)
+
+    def run(self):
+        to_drop = self.await_data()
+
+        # remove columns from to_drop
+        c.delete_columns(to_drop)
+
+        # apply model here
+
+        model_params = [1, 2, 3]
+
+        self.send_data_to_coordinator(model_params)
+
+        if self.is_coordinator:
+            return 'send_global_params'
+        else:
+            return 'apply'
+
+@app_state('send_global_params', Role.COORDINATOR)
+class SendGlobalParamsState(AppState):
+
+    def register(self):
+        self.register_transition('apply', Role.COORDINATOR)
+
+    def run(self):
+        final_params = self.gather_data()
+
+        self.broadcast_data(final_params)
+
+        return 'apply'
+
+
+@app_state('apply', Role.BOTH)
+class ApplyState(AppState):
+
+    def register(self):
+        self.register_transition('terminal')
+
+    def run(self):
+        final_params = self.await_data()
+
+        self.log(final_params)
+
+        return 'terminal'
