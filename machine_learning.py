9434c2c741c09910c41c9a906ef534fafb17f8ba
Roman Martin <web.github@online-arts.de>
2022-06-23 17:13:19 +0200

    Prototype

---
+++ machine_learning.py
@@ -0,0 +1,104 @@
+import pandas
+import numpy
+import sklearn.linear_model
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, matthews_corrcoef, f1_score, average_precision_score, precision_score, \
+    recall_score, make_scorer, confusion_matrix
+from sklearn.model_selection import train_test_split
+
+class MachineLearning():
+
+    def __init__(self):
+        pass
+
+    def prepare_data(self, df_merged: pandas.DataFrame):
+        # Total training data
+        self.X = (df_merged.loc[:, df_merged.columns != 'CLASS'])
+
+        # Target/labels array
+        self.y = (df_merged.loc[:, df_merged.columns == 'CLASS'])
+
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, stratify=self.y, test_size=0.3)
+
+    @staticmethod
+    def get_metrics(y_true, y_pred):
+        acc = accuracy_score(y_true, y_pred)
+        aps = average_precision_score(y_true, y_pred)
+        f1 = f1_score(y_true, y_pred)
+        mcc = matthews_corrcoef(y_true, y_pred)
+        prs = precision_score(y_true, y_pred)
+        recall = recall_score(y_true, y_pred)
+        cm = confusion_matrix(y_true, y_pred)
+        return "Accuracy: %.3f, Precision: %.3f, Recall: %.3f, APS: %.3f, F1: %.3f, MCC: %.3f, CM: %s" % (acc, prs, recall, aps, f1, mcc, cm)
+
+
+class FLLogisticRegression(MachineLearning):
+
+    def __init__(self):
+        self.intercepts = []
+        self.coefs = []
+        self.glob_intercept = 0
+        self.glob_coef = []
+        self.ml = None
+
+    def fit(self):
+        lr = LogisticRegression()
+        lr.fit(self.X_train, self.y_train)
+        self.ml = lr
+        self.coefs.append(lr.coef_)
+        self.intercepts.append(lr.intercept_)
+        self.glob_intercept = self.ml.intercept_
+        self.glob_coef = self.ml.coef_
+
+    def benchmark(self, X=None, y=None, check_them_all=False) -> str:
+        if X is None and y is None:
+            #print(self.X_test)
+            #print(self.y_test)
+            #print("### Params")
+            #print(self.ml.intercept_)
+            #print(self.ml.coef_)
+            if check_them_all:
+                y_pred = self.ml.predict(self.X)
+                bench = MachineLearning.get_metrics(self.y, y_pred)
+            else:
+                y_test_pred = self.ml.predict(self.X_test)
+                bench = MachineLearning.get_metrics(self.y_test, y_test_pred)
+        else:
+            y_test_pred = self.ml.predict(self.X)
+            bench = MachineLearning.get_metrics(self.y, y_test_pred)
+        return bench
+
+    def get_params(self):
+        return self.glob_intercept, self.glob_coef
+
+    def set_params(self, intercept, coef, collection=True):
+        if collection:
+            self.intercepts.append(intercept)
+            self.coefs.append(coef)
+        else:
+            self.glob_intercept = intercept
+            self.glob_coef = [coef]
+
+    def aggragate_params(self):
+        self.glob_intercept = [sum(self.intercepts) / len(self.intercepts)]
+        self.glob_coef = []
+
+        t = self.coefs
+
+        for outer in range(0, len(t[0])):
+            s = 0
+            for inner in range(0, len(t)):
+                s += t[inner][outer]
+            self.glob_coef.append(s / len(t))
+
+        return self.glob_intercept, self.glob_coef
+
+    def build_new_model(self):
+        new_lr = LogisticRegression()
+        new_lr.coef_ = numpy.array(self.glob_coef)
+        new_lr.intercept_ = self.glob_intercept
+        new_lr.classes_ = numpy.array([0, 1])
+        #print("I've a new model!!!")
+        #print(new_lr.intercept_)
+        #print(new_lr.coef_)
+        self.ml = new_lr
--- states.py
+++ states.py
@@ -1,35 +1,32 @@
-from dataclasses import dataclass
 from FeatureCloud.app.engine.app import AppState, app_state, Role
-import pandas as pd
-from pyparsing import common
-from preprocessing import analyse_data
-from logistic_regression import logistic_regression
+
+
+import preprocessing
 from collections import Counter
+
 
 # FeatureCloud requires that apps define the at least the 'initial' state.
 # This state is executed after the app instance is started.
+from machine_learning import FLLogisticRegression
 
-class Data():
+
+class DataStorage():
     # number of contributors
-    n = 1
+    contributors = 1
+    iterations = 0
 
     def read_data(self, file_anno, file_exp):
-        anno = pd.read_csv(file_anno, delimiter=',')
-        exp = pd.read_csv(file_exp, delimiter=',')
+        self.df = preprocessing.Preprocessing.read_data(file_anno, file_exp)
 
-        df = analyse_data(anno, exp)
-
-        self.df = df
-    
     def get_dataframe(self):
         return self.df
-    
+
     def delete_columns(self, to_drop):
         self.df = self.df.drop(columns=to_drop)
 
     def is_contributing(self):
-        counter = Counter(self.df['health_status'])
-        ratio = counter['P'] / (counter['P'] + counter['H'])
+        counter = Counter(self.df['CLASS'])
+        ratio = counter[0] / (counter[0] + counter[1])
 
         if ratio > 0.85:
             return False
@@ -37,16 +34,18 @@ class Data():
             return True
 
 
-c = Data()
+c = DataStorage()
+
 
 @app_state('initial')
 class InitialState(AppState):
 
     def register(self):
         self.register_transition('read', Role.BOTH)
-        
+
     def run(self):
         return 'read'
+
 
 @app_state('read', Role.BOTH)
 class ReadState(AppState):
@@ -55,7 +54,7 @@ class ReadState(AppState):
         self.register_transition('aggregate_contributions', Role.COORDINATOR)
         self.register_transition('send_empty_columns', Role.BOTH)
         self.register_transition('await', Role.BOTH)
-            
+
     def run(self):
         c.read_data('/mnt/input/anno.csv', '/mnt/input/exp.csv')
 
@@ -66,22 +65,25 @@ class ReadState(AppState):
         if self.is_coordinator:
             return 'aggregate_contributions'
         elif not self.is_coordinator and c.is_contributing():
+            print("# i contributing")
             return 'send_empty_columns'
         else:
+            print("# I'M NOT CONTRIBUTING!")
             return 'await'
 
 
 @app_state('aggregate_contributions', Role.COORDINATOR)
 class AggregateContributinsState(AppState):
-    
+
     def register(self):
         self.register_transition('send_empty_columns', Role.COORDINATOR)
-        
+
     def run(self):
         contributions = len([el for el in self.gather_data() if el == True])
-        c.n = contributions
+        c.contributors = contributions
 
         return 'send_empty_columns'
+
 
 @app_state('send_empty_columns', Role.BOTH)
 class AggregateState(AppState):
@@ -98,7 +100,8 @@ class AggregateState(AppState):
         if self.is_coordinator:
             return 'aggregate_columns_to_drop'
         else:
-            return 'await' 
+            return 'await'
+
 
 @app_state('aggregate_columns_to_drop', Role.COORDINATOR)
 class AggregateState(AppState):
@@ -107,7 +110,7 @@ class AggregateState(AppState):
         self.register_transition('await', Role.COORDINATOR)
 
     def run(self):
-        to_drop = self.await_data(n=c.n)
+        to_drop = self.await_data(n=c.contributors)
 
         self.log('received data from')
         self.log(len(to_drop))
@@ -117,6 +120,7 @@ class AggregateState(AppState):
         self.broadcast_data(common_to_drop)
 
         return 'await'
+
 
 @app_state('await', Role.BOTH)
 class AwaitState(AppState):
@@ -131,14 +135,20 @@ class AwaitState(AppState):
         c.delete_columns(common_to_drop)
 
         # apply model here
-        model_params = logistic_regression(c.get_dataframe())
+        ml = FLLogisticRegression()
+        ml.prepare_data(c.get_dataframe())
+        ml.fit()
+        print(ml.benchmark())
+        print( ml.benchmark(None, None, True) )
+        params = ml.get_params()
 
-        self.send_data_to_coordinator(model_params)
+        self.send_data_to_coordinator(params)
 
         if self.is_coordinator:
             return 'send_global_params'
         else:
             return 'apply'
+
 
 @app_state('send_global_params', Role.COORDINATOR)
 class SendGlobalParamsState(AppState):
@@ -147,13 +157,21 @@ class SendGlobalParamsState(AppState):
         self.register_transition('apply', Role.COORDINATOR)
 
     def run(self):
-        final_params = self.gather_data()
+        all_params = self.await_data(n=c.contributors)
+        print("Number of contributors: %i" % c.contributors)
+        #print(all_params)
+        ml = FLLogisticRegression()
+        for a in all_params:
+            ml.set_params(a[0][0], a[1][0])
+        ml.aggragate_params()
+        final_params = ml.get_params()
 
         # params mean
-
+        #print(final_params)
         self.broadcast_data(final_params)
 
         return 'apply'
+
 
 @app_state('apply', Role.BOTH)
 class ApplyState(AppState):
@@ -163,7 +181,13 @@ class ApplyState(AppState):
 
     def run(self):
         final_params = self.await_data()
-
-        self.log(final_params)
+        #print("#### FINAL DATA #####")
+        #print(final_params)
+        ml = FLLogisticRegression()
+        ml.set_params(final_params[0], final_params[1], False)
+        print("### NEW MODEL ###")
+        ml.build_new_model()
+        ml.prepare_data(c.get_dataframe())
+        print( ml.benchmark(None, None, True) )
 
         return 'terminal'
